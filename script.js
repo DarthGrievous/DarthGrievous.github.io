2d2c5cb0706a18eba43e297ecedbec3e0369ba58
DarthGrievous <96461524+DarthGrievous@users.noreply.github.com>
2023-10-10 01:57:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,10 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var certificatesLink = document.querySelector('a[href="certificates.html"]');
-    var profileSection = document.getElementById("about");
-
-    function showCertificates() {
-        document.getElementById("social-links").style.display = "none";
-        document.getElementById("certificates").style.display = "block";
-        profileSection.style.display = "none"; // Hide the profile section
-        certificatesLink.classList.add("active");
-    }
-
-    function showHomePage() {
-        document.getElementById("social-links").style.display = "block";
-        document.getElementById("certificates").style.display = "none";
-        profileSection.style.display = "block"; // Show the profile section
-        certificatesLink.classList.remove("active");
-    }
-
-    certificatesLink.addEventListener("click", function (event) {
-        showCertificates();
-        event.preventDefault();
+document.addEventListener("DOMContentLoaded", function() {
+    const orgBoxes = document.querySelectorAll(".organization-box");
+    
+    orgBoxes.forEach(box => {
+        box.addEventListener("click", function() {
+            const link = box.getAttribute("data-link");
+            window.location.href = link;
+        });
     });
-
-    document.querySelector('a[href="index.html"]').addEventListener("click", function (event) {
-        showHomePage();
-        event.preventDefault();
-    });
-
-    showHomePage();
 });
