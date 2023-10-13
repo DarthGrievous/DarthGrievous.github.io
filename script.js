2954c01532030ddf5c884278df86e52ff31770ba
DarthGrievous <96461524+DarthGrievous@users.noreply.github.com>
2023-10-13 04:05:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const orgBoxes = document.querySelectorAll(".organization-box");
-    
-    orgBoxes.forEach(box => {
-        box.addEventListener("click", function() {
-            const link = box.getAttribute("data-link");
-            window.location.href = link;
-        });
-    });
+document.addEventListener("DOMContentLoaded", function () {
+    function hideAllSections() {
+        // You can expand this list as per the sections you add
+        document.getElementById("home-page").style.display = "none";
+        document.getElementById("certificates-container").style.display = "none";
+        document.getElementById("projects-section").style.display = "none";
+    }
+
+    function showHomePage() {
+        hideAllSections();
+        document.getElementById("home-page").style.display = "block";
+    }
+
+    function showCertificateHub() {
+        hideAllSections();
+        document.getElementById("certificates-container").style.display = "block";
+    }
+
+    function showProjectsPage() {
+        hideAllSections();
+        document.getElementById("projects-section").style.display = "block";
+    }
+
+    // Global functions to be accessed from the HTML
+    window.showHomePage = showHomePage;
+    window.showCertificateHub = showCertificateHub;
+    window.showProjectsPage = showProjectsPage;
+
+    // Show home page by default
+    showHomePage();
 });
